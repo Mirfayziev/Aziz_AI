9f832ca88d492f69664c75e94f297af13f860f13
Aziz_Fayziev <fayziev978@mail.ru>
2025-12-04 18:04:20 +0500

    Update server.py

--- telegram_bot/server.py
+++ telegram_bot/server.py
@@ -3,12 +3,12 @@ from bot import handle_update
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"status": "telegram bot running"}
+
 @app.post("/")
 async def webhook(request: Request):
     update = await request.json()
     handle_update(update)
     return {"ok": True}
-
-@app.get("/")
-def home():
-    return {"status": "telegram bot running"}
