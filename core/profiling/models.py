d6c4bbe2e0cf869ff9ae490e1663bf08757d7199
Aziz_Fayziev <fayziev978@mail.ru>
2025-12-05 13:41:48 -0800

    Update models.py

--- core/profiling/models.py
+++ core/profiling/models.py
@@ -2,11 +2,9 @@ from sqlalchemy import Column, String, JSON
 from core.database import Base
 from core.utils import gen_id
 
-class UserProfile(Base):
-    __tablename__ = "user_profile"
+class ProfilingEvent(Base):
+    __tablename__ = "profiling_events"
 
     id = Column(String, primary_key=True, default=gen_id)
-    identity = Column(JSON)        # ism, yosh, kasb...
-    behavior = Column(JSON)        # fe’l-atvor, gap ohangi
-    preferences = Column(JSON)     # qiziqishlar, sevimli mavzular
-    stats = Column(JSON)           # qancha marta gaplashgan, so‘nggi faollik
+    event = Column(String)
+    metadata = Column(JSON)
