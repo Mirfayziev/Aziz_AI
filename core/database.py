47fbdec83b540b20c8f4ffcb06da28b039a9e560
Aziz_Fayziev <fayziev978@mail.ru>
2025-12-05 12:52:43 -0800

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,21 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from .config import settings
+from sqlalchemy.orm import sessionmaker, declarative_base
+from core.config import settings
 
-connect_args = {}
-if settings.DB_URL.startswith("sqlite"):
-    connect_args["check_same_thread"] = False
-
-engine = create_engine(
-    settings.DB_URL,
-    echo=settings.DEBUG,
-    connect_args=connect_args
-)
-
+engine = create_engine(settings.DB_URL, echo=False, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
