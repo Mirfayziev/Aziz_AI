9efa555dfb3a55c32fc49f8f03be3c4994ccf586
Aziz_Fayziev <fayziev978@mail.ru>
2025-12-05 10:48:45 -0800

    Update profile.py

--- backend/app/routers/profile.py
+++ backend/app/routers/profile.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from ..db import get_db
+from app.db import get_db
 from ..schemas import ProfileRequest, ProfileResponse
 from ..services.profile_service import update_profile
 
