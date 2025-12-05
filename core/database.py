6402143ecaaafb202708acf964aa75298700971a
Aziz_Fayziev <fayziev978@mail.ru>
2025-12-05 13:11:31 -0800

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,15 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from core.config import settings
+from core.config import DATABASE_URL
 
-engine = create_engine(settings.DB_URL, echo=False, future=True)
+engine = create_engine(DATABASE_URL, echo=False)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Backend boshlanganda chaqiriladigan funksiya
+def create_db_and_tables():
+    import core.models  # barcha modellaring shu yerga ulanadi
+    Base.metadata.create_all(bind=engine)
