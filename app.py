e70481f2136b6337e46dbd6f06b61edd6b47a35f
Aziz_Fayziev <fayziev978@mail.ru>
2025-12-06 13:51:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from core.database import create_db_and_tables
 from core.memory.router import router as memory_router
 from core.personality import personality_router
 from core.profiling.router import profiling_router
-from core.chat.router import router as chat_router
+from core.chat.router import chat_router
 from core.context.router import router as context_router
 from core.longterm.router import router as longterm_router
 from core.shortterm.router import router as short_router
